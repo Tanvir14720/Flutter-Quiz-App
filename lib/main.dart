5491166e6a666cfa45d7c37e79cc03bc305a5358
Tanvir14720 <mashrur15-14720@diu.edu.bd>
2024-05-01 22:03:02 +0600

    add-question-screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import '/widgets/add_question_screen.dart';
 import '/screens/quiz_screen.dart';
+import 'package:quiz_app_tutorial/widgets/next_button.dart';
 
 void main() {
   runApp(const MainApp());
@@ -16,6 +17,67 @@ class MainApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       theme: ThemeData.dark().copyWith(),
       home: const QuizScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RectangularButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => QuizScreen()),
+                );
+              },
+              label: 'Start Quiz',
+            ),
+            SizedBox(height: 24),
+            RectangularButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => AddQuestionScreen(),
+                  ),
+                );
+              },
+              label: 'Add Question',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShareApp extends StatefulWidget {
+  const ShareApp({Key? key}) : super(key: key);
+
+  @override
+  State<ShareApp> createState() => _ShareAppState();
+}
+
+class _ShareAppState extends State<ShareApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: (() {
+              //Share.share("");
+            }),
+            child: const Text("Share App")),
+      ),
     );
   }
 }
